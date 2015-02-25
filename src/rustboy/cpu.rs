ba26ae0c4735b5d97e2d83b039dcfcd36b120a6f
whitenoisernd <whitenoisernd@gmail.com>
2015-02-24 19:06:59 -0600

    z80 cpu skeleton done

--- src/rustboy/cpu.rs
+++ src/rustboy/cpu.rs
@@ -13,8 +13,11 @@ struct Z80_Clock;
 struct Z80_CPU;
 struct Z80_Registers;
 
+// The Z80 keeps two internal clocks, m and t. t is a measure of CPU cycles
+// and m is 1/4 of t.
 struct Z80_Clock {
-
+    m: u8,
+    t: u8
 }
 
 /*
@@ -45,4 +48,33 @@ struct Z80_Registers {
     pc: u16     // program counter / instruction pointer
 }
 
-struct Z80_CPU;
+struct Z80_CPU {
+    clock: Z80_Clock,
+    //mem: memory,
+    regs: Z80_Registers,
+}
+
+impl Z80_CPU {
+    pub fn reset(&mut self) {
+        // clear 8-bit registers
+        self.regs.a = 0;
+        self.regs.b = 0;
+        self.regs.c = 0;
+        self.regs.d = 0;
+        self.regs.e = 0;
+        self.regs.h = 0;
+        self.regs.l = 0;
+        // clear 16-bit registers
+        self.regs.af = 0;
+        self.regs.bc = 0;
+        self.regs.de = 0;
+        self.regs.hl = 0;
+        // clear pointers
+        self.regs.sp = 0; // beginning of stack
+        self.regs.pc = 0; // back to first instruction
+
+        // reset the clock
+        self.clock.m = 0;
+        self.clock.t = 0;
+    }
+}
